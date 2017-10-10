4ab5836ca85ee3b46f59579cc43addd407a267e3
nawawn <nawawn@googlemail.com>
2017-10-10 22:29:02 +0100

    update the help content

--- CollectServices.ps1
+++ CollectServices.ps1
@@ -1,21 +1,21 @@
-
-
 <#
 .Synopsis
-   Prepare PowerShell ISE to start coding a module.
+   Collect running services from servers.
 .DESCRIPTION
     The Script will collect Automatic Running services into a csv file with the server name as the file name.
+    The resulting file will be stored under C:\Temp\Services.
 .EXAMPLE
     .\CollectServices.ps1
 #>
-#if you have a list of servers in a text file, use this
+#if you have a list of servers in a text file, use this'
 #$Servers = (Get-Content C:\temp\Services\ServerName.txt)
 
 $Servers = (Get-ADComputer -Filter {OperatingSystem -like "*Server*"}).Name
-If (!(Test-Path -Path C:\temp\Services))
-{
-    New-Item -Path C:\temp\Services -ItemType Directory
+
+If (!(Test-Path -Path C:\Temp\Services)){
+    New-Item -Path C:\Temp\Services -ItemType Directory
 }
+
 foreach ($Node in $Servers) {
     If(Test-Connection $Node -Count 1 -Quiet){
         Write-Output "$Node is Online."
