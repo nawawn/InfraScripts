758b3731119f5a493e22aa97fe7dc317aac55c1a
Naw Awn <nawawn@googlemail.com>
2017-10-10 22:52:44 +0100

    Check running services across multiple servers
    Please use CollectServices.ps1 and collect services status first before running the check against the servers.

---
+++ ServicesCheck.tests.ps1
@@ -0,0 +1,46 @@
+﻿<# 
+.Description
+    The script will check all the running services against csv files collected by 'CollectServices.ps1'.
+.Example
+    Invoke-Pester .\ServicesCheck.tests.ps1
+#>
+
+#This script is written by Naw Awn using Pester.
+
+Start-Transcript C:\Temp\Services\ServicesCheck.Log -Force
+
+$CsvPath= "C:\Temp\Services"
+ 
+Describe 'Service status check'{ 
+    Context 'Csv Files Check'{ 
+        $Files = (Get-ChildItem $CsvPath\*.csv) 
+        Foreach($file in $Files){ 
+            It "$file Size should be greater than 0KB"{ 
+                $file.length | Should BeGreaterThan 0 
+            }
+            It 'should have more than 1 row' {
+	            (Import-Csv -path $file.FullName).Count | should BeGreaterThan 1
+	        }
+        } 
+    }      
+    Context 'Server Check'{ 
+        Foreach($Computer in $Files.basename){ 
+            $PingResult = (Test-Connection $Computer -Count 1 -Quiet) 
+            It "$Computer should be up and runing"{ 
+                $PingResult | Should Be $true                 
+            }
+            
+            If (!($PingResult)) { Continue }
+            Context "$Computer Service Check"{ 
+                $Services = (Import-Csv $CsvPath\$Computer.csv)      
+                Foreach($Service in $Services){ 
+                    It "$Computer $Service should be runnning"{ 
+                        (Get-Service -ComputerName $Computer -Name $Service.Name).Status | Should Be 'Running' 
+                    } 
+                } 
+            } 
+        } 
+    } 
+}
+ 
+Stop-Transcript
