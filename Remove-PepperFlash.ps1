5248af91ffaf9a4eb8209ecfccc97768b73a3189
Naw Awn <nawawn@googlemail.com>
2018-11-15 11:53:30 +0000

    Added Remove PepperFlash script

---
+++ Remove-PepperFlash.ps1
@@ -0,0 +1,24 @@
+﻿#Requires -RunAsAdministrator
+<#
+.Synopsis
+   Remove PepperFlash under all users' AppData
+.DESCRIPTION
+   The script loops all user's subfolder and remove the unwanted version of PepperFlash for Google Chrome
+.EXAMPLE
+   Just run the .ps1 file you save as.
+   Remove-PepperFlash.ps1
+#>
+
+$VerAccepted = [System.Version]"31.0.0.0"
+
+New-Variable -Name UsersFolder -Value 'C:\Users' -Description "Constant Variable for Users folder" -Option ReadOnly
+$AllUsersFolder = Get-ChildItem $UsersFolder | where{$_.BaseName -ne 'Public'}
+$PepperFlash = 'AppData\Local\Google\Chrome\User Data\PepperFlash'
+
+Foreach ($Folder in $AllUsersFolder){
+    $UserPepperFlash = Join-Path -Path $Folder.FullName -ChildPath $PepperFlash
+    #Write-Output $UserPepperFlash
+    If (Test-Path -Path $UserPepperFlash){
+        Get-ChildItem $UserPepperFlash | where{[System.Version]$_.BaseName -lt $VerAccepted} | Remove-Item -Force -Recurse        
+    }
+}
