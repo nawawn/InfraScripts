803d230f2dae183a03ba25028abfc6770929b824
Naw Awn <nawawn@googlemail.com>
2019-03-11 15:54:11 +0000

    Added Test-Credential script

---
+++ Test-Credential.ps1
@@ -0,0 +1,14 @@
+﻿$cred = Get-Credential
+$username = $cred.username
+$password = $cred.GetNetworkCredential().password
+
+ # Get current domain using logged-on user's credentials
+$CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
+$domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$UserName,$Password)
+ 
+if ($domain.name -eq $null){
+    Write-Output "Authentication failed - please verify your username and password." 
+}
+else{
+    Write-Output "Successfully authenticated with domain $($domain.name)"
+}
