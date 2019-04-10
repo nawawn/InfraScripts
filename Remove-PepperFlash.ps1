d1741363a370d2d023a0b78dc5953a55dbce3151
Naw Awn <nawawn@googlemail.com>
2019-04-10 15:45:29 +0100

    Updated the Remove-PepperFlash

--- Remove-PepperFlash.ps1
+++ Remove-PepperFlash.ps1
@@ -4,13 +4,16 @@
 .Synopsis
    Remove PepperFlash under all users' AppData
 .DESCRIPTION
-   The script loops all user's subfolder and remove the unwanted version of PepperFlash for Google Chrome
+   The script loops all user's subfolder and remove the unwanted version of PepperFlash for Google Chrome.
+   Specify the version you would like to keep by using KeepVer parameter.
 .EXAMPLE
    Just run the .ps1 file you save as.
    Remove-PepperFlash.ps1
+.EXAMPLE   
+   Remove-PepperFlash.ps1 -KeepVer "32.0.0.0"
 #>
 Param(
-    [System.Version]$SustainedVer = "31.0.0.0"
+    [System.Version]$KeepVer = "31.0.0.0"
 )
 
 If (!(Get-Variable -Name UsersFolder)){
@@ -23,6 +26,6 @@ Foreach ($Folder in $AllUsersFolder){
     $UserPepperFlash = Join-Path -Path $Folder.FullName -ChildPath $PepperFlash
     #Write-Output $UserPepperFlash
     If (Test-Path -Path $UserPepperFlash){
-        Get-ChildItem $UserPepperFlash | where{[System.Version]$_.BaseName -lt $SustainedVer} | Remove-Item -Force -Recurse        
+        Get-ChildItem $UserPepperFlash | where{[System.Version]$_.BaseName -lt $KeepVer} | Remove-Item -Force -Recurse        
     }
 }
