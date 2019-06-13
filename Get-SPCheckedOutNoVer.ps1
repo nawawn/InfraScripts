825456ee3fe85693ec881741075991c1306f5ad6
Naw Awn <nawawn@googlemail.com>
2019-06-13 12:36:22 +0100

    Added SharePoint Checked Out File with no checkin version script

---
+++ Get-SPCheckedOutNoVer.ps1
@@ -0,0 +1,47 @@
+[CmdletBinding()]
+Param(
+    [Switch]$CheckIn = $False
+)
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+Function Set-OwnershipCheckin{
+    Param($File)
+    Process{
+        Write-Verbose "Take ownership of the file - $($File.LeafName)"
+        $File.TakeOverCheckOut()                    
+        Write-Verbose "Check in the file - $($File.LeafName)"
+        $List.GetItemById($File.ListItemId).File.Checkin("Checked in by Administrator")        
+    }
+}
+#Site Collection Variable
+$SiteURL="http://ourfiles/bmteam/hr"
+  
+#Get the Site Collection
+$Site = Get-SPSite $SiteURL
+
+$FileList = @()
+$Property = @('LeafName','CheckedOutBy','CheckedOutByName','CheckedOutByEmail','TimeLastModified','DirName')
+
+#Loop through each sub-site of the site collection
+ForEach($Web in $Site.AllWebs){
+    Write-Output "Searching for Checked out Files with no Checked in Version at:" $Web.Url
+    #Loop through each list
+    ForEach($List in $Web.Lists) {
+        #Get Checked out files with no checkin versions
+        $CheckedOutFiles = $List.CheckedOutFiles
+ 
+        #Loop through each checked out File
+        ForEach ($File in $CheckedOutFiles){
+            If ($File){
+                $FileList += $File | Select-Object -Property $Property
+                
+                If ($CheckIn){
+                    Set-OwnershipCheckin -File $File
+                }                
+            }
+        }
+        #$FileList
+    }
+}
+$FileList | Export-Csv  -Path D:\temp\NoVersionCheckedOut.csv -NoTypeInformation
