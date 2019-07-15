324ae451bd32600f5d3b5a46391071dc9ff96730
Naw Awn <nawawn@googlemail.com>
2019-07-15 15:15:25 +0100

    Install Predefined Modules script added

---
+++ PSModules.ps1
@@ -0,0 +1,36 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(ParameterSetName='Install')]
+    [Swtich]$Install,
+    [Parameter(ParameterSetName='Update')]
+    [Swtich]$Update
+)
+
+$Modules = @'
+Az
+AzureAD
+MSOnline
+Microsoft.Online.SharePoint.PowerShell
+MicrosoftTeams
+SQLServer
+DbaTools
+ImportExcel
+Pester
+Posh-SSH
+PSSQLite
+PowerShellGet
+'@ -Split '\r\n'
+
+If ($Install){
+    Foreach ($m in $Modules){
+        Write-Verbose "Installing $m Module"
+        Install-Module -Name $m -Force -AllowClobber
+    }
+}
+
+If ($Update){
+    Foreach ($m in $Modules){
+        Write-Verbose "Updating $m Module"
+        Update-Module -Name $m -Force
+    }
+}
