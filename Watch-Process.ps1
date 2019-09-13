0c2ac6d337a69c8e0c27df164905d943abffad21
Naw Awn <nawawn@googlemail.com>
2019-09-13 17:47:19 +0100

    Added watch process send email alert

---
+++ Watch-Process.ps1
@@ -0,0 +1,35 @@
+﻿Function Send-EmailAlert{
+    Param(
+        [ValidateNotNullorEmpty()][String]$Process = "notepad",
+        [AllowNull()][PSCredential]$Credential
+    )
+    Begin{
+        $Start = Get-Date
+    }
+    Process{
+
+        While(Get-Process $Process -ErrorAction 'SilentlyContinue'){
+            Get-Process $Process | Select Handles,PM,CPU,@{n='Time';e={Get-Date}}
+            Start-Sleep 60        
+        }
+        
+        $Stop = Get-Date
+        $Msg = "The process started on $Start and finished at $Stop"
+        
+        $MailParam = @{}
+        $MailParam.Add('From',      "machine@mydomain.com")
+        $MailParam.Add('To',        "naw.awn@mydomain.com"  )
+        $MailParam.Add('Subject',   "$Process Job Completed"  )
+        $MailParam.Add('Body',       $Msg                     )
+        $MailParam.Add('SmtpServer',"ExchangeServer"              )
+        If ($Credential){ $MailParam.Add('Credential',$Credential) }
+
+        Send-MailMessage @MailParam
+        New-TimeSpan -Start $Start -End $Stop
+    }
+<#
+.EXAMPLE
+   $Cred = Get-Credential
+   Send-EmailAlert -Process 'notepad' -Credential $Cred
+#>
+}
