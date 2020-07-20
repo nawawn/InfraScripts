92053524526c87da7ceeef9681f7ea865f557625
Naw Awn <nawawn@googlemail.com>
2020-07-20 15:11:51 +0100

    Create Open-OneInstance.ps1

---
+++ Open-OneInstance.ps1
@@ -0,0 +1,33 @@
+Function Open-OneInstance{
+    Param(
+        [String]$FilePath = 'C:\Windows\system32\notepad.exe',
+        [String[]]$Argument   
+    )
+    Begin{
+        $Mutex = New-Object System.Threading.Mutex($false, [System.IO.Path]::GetFileName($FilePath))
+        $Param = @{}
+        $Param['FilePath'] = $FilePath
+        If ($Argument){ $Param['ArgumentList'] = $Argument }
+    }
+    Process{
+        try {
+            [Void]$Mutex.WaitOne()
+            Start-Process @Param -Wait
+        }
+        finally {
+            $Mutex.ReleaseMutex()
+            $Mutex.Dispose()
+        }
+    }
+<#
+.Synopsis
+   Open only one instance of an application by script 
+.DESCRIPTION
+   When running an application, this cmdlets open only one instance at a time.
+   The same application can be run outside of the script but running it from the script will limit to only one at a time.
+.EXAMPLE
+   Open-OneInstance -FilePath 'C:\Windows\system32\notepad.exe'
+.INPUTS
+   Executable file and its arguments
+#>
+}
