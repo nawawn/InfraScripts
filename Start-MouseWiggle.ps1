bd03b214088d16ce490b5a375b80795370c763b2
Naw Awn <nawawn@googlemail.com>
2020-09-22 12:20:46 +0100

    Add files via upload

---
+++ Start-MouseWiggle.ps1
@@ -0,0 +1,15 @@
+﻿Function Start-MouseWiggle{
+    Begin {
+        Add-Type -AssemblyName System.Windows.Forms
+    }
+    Process{    
+        While($true){
+            Start-Sleep -Seconds 5
+            $x = (random 1000)
+            $y = (random 1000)
+            Write-Host "->move $x,$y " -NoNewline
+            [Windows.Forms.Cursor]::Position = New-Object Drawing.Point $x,$y
+        }
+    }
+}
+Start-MouseWiggle
